b51905dec516802ec3a44c8c7cc6cea0f8f9a54d
ilonatommy <ili.ili@interia.pl>
2019-10-29 19:29:10 +0100

    Finished experiments with lab_03, vecAdd ready for experiments with lab_04

--- vec_add/src/vectorAdd.cu
+++ vec_add/src/vectorAdd.cu
@@ -52,6 +52,7 @@ vectorAdd(const float *A, const float *B, float *C, int numElements)
 int
 main(void)
 {
+
     // Error code to check return values for CUDA calls
     cudaError_t err = cudaSuccess;
 
@@ -60,7 +61,7 @@ main(void)
     int numElements = 2 << 27;
     size_t size = numElements * sizeof(float);
     printf("[Vector addition of %d elements]\n", numElements);
-
+/*
     // Allocate the host input vector A
     float *h_A = (float *)malloc(size);
 
@@ -83,11 +84,11 @@ main(void)
         h_A[i] = rand()/(float)RAND_MAX;
         h_B[i] = rand()/(float)RAND_MAX;
     }
-
+*/
     //DATA STRUCTURE DEVICE:
     // Allocate the device input vector A
     float *d_A = NULL;
-    err = cudaMalloc((void **)&d_A, size);
+    err = cudaMallocManaged((void **)&d_A, size);
 
     if (err != cudaSuccess)
     {
@@ -97,7 +98,7 @@ main(void)
 
     // Allocate the device input vector B
     float *d_B = NULL;
-    err = cudaMalloc((void **)&d_B, size);
+    err = cudaMallocManaged((void **)&d_B, size);
 
     if (err != cudaSuccess)
     {
@@ -107,14 +108,14 @@ main(void)
 
     // Allocate the device output vector C
     float *d_C = NULL;
-    err = cudaMalloc((void **)&d_C, size);
+    err = cudaMallocManaged((void **)&d_C, size);
 
     if (err != cudaSuccess)
     {
         fprintf(stderr, "Failed to allocate device vector C (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-
+/*
     // Copy the host input vectors A and B in host memory to the device input vectors in
     // device memory
     printf("Copy input data from the host memory to the CUDA device\n");
@@ -133,7 +134,7 @@ main(void)
         fprintf(stderr, "Failed to copy vector B from host to device (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-
+*/
     //DEFINING GRID LAYOUT:
     // Launch the Vector Add CUDA Kernel
     int threadsPerBlock = 1024;
@@ -151,7 +152,7 @@ main(void)
         fprintf(stderr, "Failed to launch vectorAdd kernel (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-
+/*
     // Copy the device result vector in device memory to the host result vector
     // in host memory.
     printf("Copy output data from the CUDA device to the host memory\n");
@@ -162,11 +163,11 @@ main(void)
         fprintf(stderr, "Failed to copy vector C from device to host (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-
+*/
     // Verify that the result vector is correct
     for (int i = 0; i < numElements; ++i)
     {
-        if (fabs(h_A[i] + h_B[i] - h_C[i]) > 1e-5)
+        if (fabs(d_A[i] + d_B[i] - d_C[i]) > 1e-5)
         {
             fprintf(stderr, "Result verification failed at element %d!\n", i);
             exit(EXIT_FAILURE);
@@ -199,12 +200,12 @@ main(void)
         fprintf(stderr, "Failed to free device vector C (error code %s)!\n", cudaGetErrorString(err));
         exit(EXIT_FAILURE);
     }
-
+/*
     // Free host memory
     free(h_A);
     free(h_B);
     free(h_C);
-
+*/
     printf("Done\n");
     return 0;
 }
