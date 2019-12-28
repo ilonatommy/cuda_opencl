19d01578c1b4c61bf71451324a15065e647d36e4
Racuszek <zofia.pienkowska@samorzad.agh.edu.pl>
2019-12-28 14:32:04 +0100

    Single-threaded matrix multiplication now works well with single pointer matrices

--- lab_06/single-threaded_mult/single_mult_v2.c
+++ lab_06/single-threaded_mult/single_mult_v2.c
@@ -1,45 +1,75 @@
 #include <stdio.h> 
 #include <stdlib.h>
-#define S 3
+#define S 2
 
-void PrintMatrix(float* M, int Width)
+void PrintMatrix(int* M, int Width)
 {
         for(int i = 0; i < Width; i++)
         {
                 for(int j = 0; j < Width; j++)
-                        printf("%f  ",M[i*Width+j]);
+                        printf("%d  ",M[i*Width+j]);
                 printf("\n");
         }
     printf("\n");
 }
 
-void multiply(float* M, float* N, float* P, int size) {
+void multiply(int* M, int* N, int* P, int size) {
+    int X[S][S], Y[S][S], Z[S][S];
+    int i, j;
+
+    printf("Rewriting matrices\n");
+    for (i=0;i<S;i++) {
+        for (j=0;j<S;j++) {
+            X[i][j]=M[S*i+j];
+            Y[i][j]=N[S*i+j];
+            Z[i][j]=0;
+        }
+    }
     
+    // Multiplying first and second matrices and storing in Z.
+    for (i = 0; i < S; ++i) {
+        for (j = 0; j < S; ++j) {
+            for (int k = 0; k < S; ++k) {
+                Z[i][j] += X[i][k] * Y[k][j];
+            }
+        }
+    }
+    printf("Result matrix:\n");
+    for (i=0;i<S;i++) {
+        for (j=0;j<S;j++) {
+            P[S*i+j]=Z[i][j];
+        }
+        printf("\n");
+    }
+
 
 }
 
 int main() {
-    float* M=malloc(S*S*sizeof(float)); // pierwsza macierz
-    float* N=malloc(S*S*sizeof(float)); // druga macierz
-    float* P=malloc(S*S*sizeof(float)); // macierz wynikowa
+    int* M=malloc(S*S*sizeof(int)); // pierwsza macierz
+    int* N=malloc(S*S*sizeof(int)); // druga macierz
+    int* P=malloc(S*S*sizeof(int)); // macierz wynikowa
+
 
     int i, j;
-    printf("Filling matrix M\n");
     for (i=0;i<S;i++) {
         for (j=0;j<S;j++) {
-            M[i*S+j]=(float)rand()/(float)RAND_MAX;         
+            M[i*S+j]=rand()%10   ;     
         }
     }
 
     for (i=0;i<S;i++) {
         for (j=0;j<S;j++) {
-            N[i*S+j]=(float)rand()/(float)RAND_MAX;
+            N[i*S+j]=rand()%10;
+            P[i*S+j]=0;
         }
     }
+    
 
 
     PrintMatrix(M, S);
     PrintMatrix(N, S);
+    PrintMatrix(P, S);
 
     multiply(M, N, P, S);
     PrintMatrix(P, S);
